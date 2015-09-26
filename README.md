2544cff384ed9756b3e0810dde30fa999cb1812a
Joshua Wulf <sitapati@worldsankirtan.net>
2015-09-27 04:10:26 +1000

    updated

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # awsm-loggly
-JAWS awsm module to provide logging to your lambdas
+JAWS awsm module to provide logging to your lambdas.
+
+Application code running in the cloud can be challenging to debug. 
+Loggly is a cloud-based logging service that allows you to search and filter log data. 
+Logging to Loggly from your lambdas allows you to trace application execution across multiple lambdas, and debug your application flow.
+
+To use:
+
+In your JAWS project root directory, run:
+```
+jaws module install https://github.com/jwulf/awsm-loggly
+```
+ 
+ You can specify your Loggly details in your lambda code:
+ 
+ ```
+ var logglyCredentials = {
+	token: _LOGGLY_TOKEN_,
+	subdomain: _LOGGLY_SUBDOMAIN_,
+	tags: [_LOGGLY_TAGS_]	 
+}
+	 
+ var log = require('./../../loggly').Log(logglyCredentials);
+ ```
+ 
+ or you can specify them in `.env`:
+ 
+```
+LOGGLY_TOKEN="_TOKEN_"
+LOGGLY_SUBDOMAIN="_SUBDOMAIN_"
+LOGGLY_TAGS="some tag"
+ ```
+ If you put the credentials in the `.env` file, you can instantiate the logger like this:
+ ```
+ var log = require('./../../loggly').Log({tags: [thisFunctionName]);
+ ```
+ 
+ The tags will be added to any tags that you defined in `.env`. Typically you'll want to push the name of your Lambda function, so that you can filter for it in your logs.
+ 
+ ```
+ log('trace', 'Processing a new order', {ordernum: order.number, customer: order.customerId}).then(
+	 function() {
+		 \\ Execution continues here
+});
+ ```
+ 
--- index.js
+++ index.js
@@ -2,27 +2,40 @@ var winston = require('winston');
 require('winston-loggly');
 var Promise = require('bluebird');
 
-// Replace these with your Loggly values
-Loggly = {
+// Replace these with your Loggly credentials
+var LogglyEnv = {
+    tags: process.env.LOGGLY_TAGS,
     token: process.env.LOGGLY_TOKEN,
-    subdomain: process.env.LOGGLY_SUBDOMAIN,
-    tags: [process.env.LOGGLY_TAGS]
+    subdomain: process.env.LOGGLY_SUBDOMAIN
 }
-
- winston.add(winston.transports.Loggly, {
-    token: Loggly.token,
-    subdomain: Loggly.subdomain,
-    tags: Loggly.tags,
-    json:true
-});
-
-var log = Promise.promisify(winston.log);
-
-function loginfo (msg, data, callback) {
-   log('info', msg, data).then(callback);
-}
-
-module.exports.log = log;
-module.exports.info = Promise.promisify(loginfo);
 
 var loglevel = ['info', 'warning', 'error', 'fatal', 'debug', 'trace'];
+
+module.exports.Log = function(LogglyData) {
+    var tags = [],
+        envTags = [],
+        dataTags = [];
+    if (LogglyEnv.tags) { envTags = LogglyEnv.tags; }
+    if ('string' == typeof envTags) { envTags = [envTags] };
+    if (LogglyData.tags) { dataTags = LogglyData.tags; }
+    if ('string' == typeof dataTags) { envTags = [dataTags] };   
+     
+    tags = envTags.concat(dataTags);
+    var Loggly = {
+        token: LogglyData.token || LogglyEnv.token,
+        subdomain: LogglyData.subdomain || LogglyEnv.subdomain,
+        tags: tags
+    }
+    console.log(Loggly);
+    winston.add(winston.transports.Loggly, {
+        token: Loggly.token,
+        subdomain: Loggly.subdomain,
+        tags: Loggly.tags,
+        json:true
+    });
+
+    var PromisifiedLog = Promise.promisify(winston.log);
+
+    return PromisifiedLog;
+}
+
